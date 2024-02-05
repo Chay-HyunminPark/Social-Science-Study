a6786854be2a903edd0047d122a0941833916255
Chay Hyunmin Park <123585452+Chay-HyunminPark@users.noreply.github.com>
2024-02-04 22:51:13 -0500

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,23 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from 
+
+# Purpose: Downloads and saves data from the journal article titled "The Puzzle of Falling US Birth Rates since the Great Recession" on the American Economic Association website.
+
 # Author: Chay Park
-# Date: 4 February 2024
+
+# Date: February 4, 2024
+
 # Contact: chay.park@mail.utoronto.ca
+
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
+# Pre-requisites: An OPENICPSR account is required.
 
-#### Workspace setup ####
-library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
-
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+# Instructions for Downloading the Data: Since downloading the data requires signing in, please follow the provided instructions:
+# 1. Visit the American Economic Association journal page at [AEAJournals](https://www.aeaweb.org/journals).
+# 2. Search for "Birth rates" in the search bar.
+# 3. Click on the journal article titled "The Puzzle of Falling US Birth Rates since the Great Recession".
+# 4. Scroll down to the "Additional Materials" section and click on "Replication Package".
+# 5. You will be directed to the [OPENICPSR project page](https://www.openicpsr.org/openicpsr/project/144981/version/V1/view).
+# 6. Click on "download this package" to proceed.
+# 7. Sign in with your OPENICPSR account credentials.
+# 8. Once signed in, you will be able to download the project.
