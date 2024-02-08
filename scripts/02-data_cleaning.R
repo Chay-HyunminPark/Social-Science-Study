c2d1c05f6e63e755eb3af7167e6ed28debebdd94
Chay Hyunmin Park <123585452+Chay-HyunminPark@users.noreply.github.com>
2024-02-08 00:11:09 -0500

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -7,7 +7,33 @@
 # Pre-requisites: Need to download the replication package
 
 #### Workspace setup ####
+install.packages(c("knitr", "lubridate", "tidyverse"))
 
+# See a list of available packages
+library(knitr)
+library(lubridate)
+library(tidyverse)
+
+packages <- list_packages(limit = 10)
+packages
 
 #### Clean data ####
 
+# Write CSV file for figure 1
+write_csv(
+  x = trend_us_brate,
+  file = "inputs/data/fig_1.csv"
+)
+head(trend_us_brate)
+
+# Write CSV file for figure 2
+write_csv(
+  x = numbirth,
+  file = "inputs/data/decomp/births_educ_race_age6.csv"
+)
+
+# Write CSV file for figure 3
+write_csv(
+  x = state_birth,
+  file = "inputs/data/map/state_birth_04_19.csv"
+)
