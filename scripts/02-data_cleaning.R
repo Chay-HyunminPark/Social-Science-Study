46283c7e6b14917591c61053af526f17625dca26
Chay Hyunmin Park <123585452+Chay-HyunminPark@users.noreply.github.com>
2024-02-15 00:36:35 -0500

    Update 02-data_cleaning.R

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -2,18 +2,7 @@
 # Purpose: To sort out relevant necessary data from the data set 
 # ~not sure whether I should haave this script tho
 # Author: Chay Park
-# Date: 4 February 2024
+# Date: 15 February 2024
 # Contact: chay.park@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: Need to download the replication package
-
-#### Workspace setup ####
-# install.packages(c("knitr", "lubridate", "tidyverse"))
-
-# See a list of available packages
-library(knitr)
-library(lubridate)
-library(tidyverse)
-
-#### Clean data ####
-
