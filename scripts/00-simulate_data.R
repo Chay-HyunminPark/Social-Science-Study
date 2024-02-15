eb34334f7cfd6072e211257a4a249e0466c39411
Chay Hyunmin Park <123585452+Chay-HyunminPark@users.noreply.github.com>
2024-02-15 01:36:57 -0500

    Update 00-simulate_data.R

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,8 +1,7 @@
 #### Preamble ####
 # Purpose: The purpose of this script is to simulate and visualize the trend in birth rates in the United States from the year 1980 
 # to 2020. The script generates tables and graphs to illustrate the overall trend in US birth rates, as well as the birth rates among 
-# various subgroups such as different age groups, ethnicities, educational levels, and marital statuses. 
-# Additionally, the script creates a state-level map to visualize the change in birth rates across different states over the specified period.
+# various subgroups such as different age groups and ethnicities
 # Author: Chay Park
 # Date: 3 February 2024 
 # Contact: chay.park@mail.utoronto.ca
